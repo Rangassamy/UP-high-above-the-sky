a84e90ded9b3cf2c3e6dddd340a8756b6c7f2b02
MATHIP6 <116129737+MATHIP6@users.noreply.github.com>
2026-02-06 07:40:53 -0400

    Rename orders

--- backend/src/routes/order.py
+++ backend/src/routes/order.py
@@ -7,14 +7,14 @@ from src.core.security import get_current_user, is_admin
 router = APIRouter()
 
 
-@router.get("/orders")
+@router.get("/orders/me")
 async def get_all_user_orders(access_token: Annotated[str | None, Cookie()]):
     user = await get_current_user(access_token)
     orders = crud.get_all_by_user_id(user.id)
     return orders
 
 
-@router.get("/orders/admin")
+@router.get("/orders")
 async def get_all(access_token: Annotated[str | None, Cookie()]):
     user = await get_current_user(access_token)
     await is_admin(user)
